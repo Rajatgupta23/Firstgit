a697b567796f1de3f7e42570241e7493b4fd7d2b
Rajatgupta23 <rajatccmt@gmail.com>
2023-02-02 10:29:33 +0530

    second_commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+console.log(document.domain)
+console.log(document.URL)
+
+console.log(document.title)
+
+document.title= 123
+console.log(document.head)
+console.log(document.body)
+console.log(document.doctype)
+console.log(document.all[1])
+console.log(document.getElementById("items"))
+var item= document.getElementsByClassName("list-group")
+console.log(items)
